831dc065d96994452a54b5ee3088f123376be85f
rosu123 <rosu.perela@gmail.com>
2018-04-08 01:20:50 +0200

    acortador de URLs (en principio) terminado

--- myproject/urls.py
+++ myproject/urls.py
@@ -4,8 +4,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'practica2.views.mostrar_info'),
-    #url(r'^cms/$', 'practica2.views.mostrar_info'),
+    url(r'^$', 'practica2.views.view_info'),
     url(r'^(\d+)', 'practica2.views.content'),
     url(r'^(.*)', 'practica2.views.msg_error'),
 ]
